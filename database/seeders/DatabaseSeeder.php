a2816d6b64d5cf0c86a09f3e1ea627405f5392b4
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-05-08 22:46:52 +0300

    done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
             CurrencySeeder::class,
             RoleSeeder::class,
             UsersWithRolesSeeder::class,
-            CategoriesSeeder::class,
         ]);
     }
 }
