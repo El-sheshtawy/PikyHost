af8a4018d40a73b52ce1f509d83efc23b5b0c8d2
Mohamed Elsheshtawy <ramyalfe22@gmail.com>
2025-05-09 14:22:46 +0300

    done

--- app/Filament/Resources/ProjectResource/Pages/ViewProject.php
+++ app/Filament/Resources/ProjectResource/Pages/ViewProject.php
@@ -3,10 +3,15 @@
 namespace App\Filament\Resources\ProjectResource\Pages;
 
 use App\Filament\Resources\ProjectResource;
-use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewProject extends ViewRecord
 {
     protected static string $resource = ProjectResource::class;
+
+    public static function getRelations(): array
+    {
+        return [
+        ];
+    }
 }
